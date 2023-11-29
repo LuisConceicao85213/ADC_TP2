16ac06037cc92ac814e85bce2e173be97dd9754d
David Marques a85229 <a85229@ualg.pt>
2023-11-29 20:46:00 +0000

    criar_funçao_pregunta

--- src/io_terminal.py
+++ src/io_terminal.py
@@ -8,3 +8,24 @@ from tabulate import tabulate
 # todos os outros ficheiros io_terminal-*.py, e inclusive estes comentários
 
 # ...
+
+def pergunta_id(questao, lista, mostra_lista=False):
+    """
+    :param questao: Pergunta
+    :ParaType:Str
+    :param lista: É uma lista com um cabeçalho
+    :ParaType:Int
+    :param mostra_lista: Serve para imprimir lista na interface
+    :ParaType:Bool
+    :return: id filtrado
+    """
+    if mostra_lista:
+        imprime_lista(cabecalho="", lista=lista)
+
+    while True:
+        id = int(input(questao))
+        if 0 <= id < len(lista):
+            return id
+        else:
+            print(f"id inexistente. Tente de novo. Valores admitidos {0} - {len(lista)}")
+
